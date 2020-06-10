15f4d3ca057da4e468f45bef3a421fededd58440
zecho <zakel@foxmail.com>
2020-06-10 22:44:38 +0800

    测试@ModelAttribute及@SessionAttribute注解用法

--- src/main/java/com/lizard/simpleweb/contoller/HelloContoller.java
+++ src/main/java/com/lizard/simpleweb/contoller/HelloContoller.java
@@ -1,9 +1,11 @@
 package com.lizard.simpleweb.contoller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.lizard.simpleweb.bean.User;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @author x
@@ -21,18 +23,42 @@ public class HelloContoller {
         System.out.println("getHello!" + userid);
     }
 
-    @GetMapping("/hello/{userid}")
-    public void getMapInfo(@PathVariable("userid") String userid) {
-        System.out.println("getHello!" + userid);
+    /**
+     * 直接从Session中读取指定属性
+     */
+    @GetMapping("/hello/getSession")
+    public void testGetSessionAttribute(@SessionAttribute("user") User user) {
+        System.out.println("user = " + user);
     }
 
-    @GetMapping("/hello/{userid}")
-    public void getPojoInfo(@PathVariable("userid") String userid) {
-        System.out.println("getHello!" + userid);
+    /**
+     * 设置session属性
+     */
+    @GetMapping("/hello/setAttr")
+    public void setSessionAttr(HttpSession session) {
+        User user = new User();
+        user.setName("lina");
+        user.setEmail("zak@gmail.com");
+        session.setAttribute("user", user);
     }
 
+    /**
+     * 绑定属性
+     */
     @ModelAttribute
-    public void getParamsMap(){
-
+    public void bindAttr(Model model) {
+        User user = new User();
+        user.setName("jaky");
+        user.setEmail("jaky@gmail.com");
+        model.addAttribute("userb", user);
     }
+
+    /**
+     * 直接通过ModelAttribute获取绑定的属性
+     */
+    @GetMapping("/hello/getModelAttr")
+    public void getModelAttribute(@ModelAttribute("userb") User user) {
+        System.out.println("user = " + user);
+    }
+
 }
