38615964e1c34bd87611d42ffd02171b73bd5ff1
zecho <zakel@foxmail.com>
2020-07-28 00:10:17 +0800

    增加controller、task模块，删掉doc目录

--- simpleweb-util/src/test/java/SensitiveDataTest.java
+++ simpleweb-util/src/test/java/SensitiveDataTest.java
@@ -21,15 +21,15 @@ public class SensitiveDataTest {
 
     private static final String JSON_REPLACEMENT_REGEX = "$0: ****";
     private static final String JSON_KEYS =
-        "[" + StringUtils.join(new String[] {"ssn", "private", "creditCard"}, '|') + "]";
+            "[" + StringUtils.join(new String[]{"ssn", "private", "creditCard"}, '|') + "]";
     private static final Pattern JSON_PATTERN = Pattern.compile(JSON_KEYS + ": (\\S+)");
 
     @Test
     public void testMaskString() {
         String s = SensitiveUtil.maskFieldString();
         System.out.println("s = " + s);
-        String s1 = "[" + StringUtils.join(new String[] {"ssn", "private", "creditCard"}, '|') + "]";
-        System.out.println("s1 = " + s1);;
+        String s1 = "[" + StringUtils.join(new String[]{"ssn", "private", "creditCard"}, '|') + "]";
+        System.out.println("s1 = " + s1);
         String s2 = ": (\\S+)";
     }
 
@@ -93,5 +93,12 @@ public class SensitiveDataTest {
 
         SensitiveUtil.jsonFieldFilter(log);
         System.out.println("log.toString() = " + log.toString());
+    }
+
+    @Test
+    public void testRegexp() {
+        String test = "abcbbabcbcgbddesddfiid";
+        String reg = "(a)(b)c";
+        System.out.println(test.replaceAll(reg, "$1"));
     }
 }
